71ecd0e6b7788bff8ca250f00be6bdfb5a70b96f
Alexander Schilling <alexander.schilling.miranda@gmail.com>
2022-07-30 00:19:06 -0400

    refactor: remove uppercase folder name

--- src/Pages/ThirdPage.tsx
+++
@@ -1,9 +0,0 @@
-function ThirdPage() {
-    return (
-      <div>
-        <h2>Esta es otra pagina secundaria</h2>
-      </div>
-    );
-  }
-  
-  export default ThirdPage;
--- src/Pages/index.tsx
+++
@@ -1,7 +0,0 @@
-import SecondPage from './SecondPage'
-import ThirdPage from './ThirdPage'
-
-export {SecondPage, ThirdPage}
-
-// This file allows to import multiple components from the parent folder
-// We use this feature in the file "src/index.tsx"
