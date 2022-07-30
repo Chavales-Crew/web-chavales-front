36090105059448db3d2ca684cc8d28ee8850252a
BrantDesu <chamorrobrant@gmail.com>
2022-07-29 21:10:54 -0400

    new: added routing to the app

---
+++ src/Pages/ThirdPage.tsx
@@ -0,0 +1,9 @@
+function ThirdPage() {
+    return (
+      <div>
+        <h2>Esta es otra pagina secundaria</h2>
+      </div>
+    );
+  }
+  
+  export default ThirdPage;
---
+++ src/Pages/index.tsx
@@ -0,0 +1,7 @@
+import SecondPage from './SecondPage'
+import ThirdPage from './ThirdPage'
+
+export {SecondPage, ThirdPage}
+
+// This file allows to import multiple components from the parent folder
+// We use this feature in the file "src/index.tsx"
