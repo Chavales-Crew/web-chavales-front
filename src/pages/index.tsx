44cd59bf9f4bad26a1d2c687cf3904310e5a13c1
Alexander Schilling <19366689+alexander-schilling@users.noreply.github.com>
2022-07-30 00:33:13 -0400

    Revert "Feat/cambio de nombre pagina"

--- src/pages/index.tsx
+++
@@ -1,8 +0,0 @@
-import LandingPage from "./LandingPage";
-import SecondPage from "./SecondPage";
-import ThirdPage from "./ThirdPage";
-
-export { LandingPage, SecondPage, ThirdPage };
-
-// This file allows to import multiple components from the parent folder
-// We use this feature in the file "src/index.tsx"
